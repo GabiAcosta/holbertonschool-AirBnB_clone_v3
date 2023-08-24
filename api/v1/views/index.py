e66961e7de9f6f1cdb44138f50f566d5b0a40ba6
GabiAcosta <acostagabriel1311@gmail.com>
2023-08-24 05:35:27 -0700

    task 5 - created endpoint that returns the numbear of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,8 +7,26 @@ indicating the status of the application.
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status")
 def status():
     return jsonify(status="OK")
+
+@app_views.route("/stats")
+def stats():
+    amenities_count = storage.count('Amenity')
+    cities_count = storage.count('City')
+    places_count = storage.count('Place')
+    reviews_count = storage.count('Review')
+    states_count = storage.count('State')
+    users_count = storage.count('User')
+    return jsonify(
+        amenities=amenities_count,
+        cities=cities_count,
+        places=places_count,
+        reviews=reviews_count,
+        states=states_count,
+        users=users_count
+    )
