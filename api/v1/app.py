26716e507478c583caf594f1fcb0df2f99ff01b0
Faqu22 <facundoalvarez415@gmail.com>
2023-08-25 09:41:43 -0300

    Implementation of flask_cors package

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,26 +1,22 @@
-#!/usr/bin/python3
-"""
-starts a Flask web application
-"""
 from flask import Flask, jsonify
 from models import storage
 from os import getenv
 from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
+app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
 
-
 @app.errorhandler(404)
 def error_404(exception):
     return jsonify(error="Not found"), 404
-
 
 if __name__ == '__main__':
     if getenv("HBNB_API_HOST"):
